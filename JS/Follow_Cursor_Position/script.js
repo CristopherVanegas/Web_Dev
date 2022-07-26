8f668d31106c608d23f421b2ee70883fa3eee8d4
CristopherVanegas <cristopher.platzi@gmail.com>
2022-07-26 07:55:43 -0500

    ADDED first part of js script

---
+++ JS/Follow_Cursor_Position/script.js
@@ -0,0 +1,9 @@
+let circle = document.querySelector('.circle');
+const moveCircle = (e) => {
+    circle.style.left = `${e.pageX}px`;
+    circle.style.top = `${e.pageY}px`;
+}
+
+const addPulse = () => {
+    
+}
