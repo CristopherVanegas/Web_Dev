06b7b92b81dfb36f9df14ef9c0c8eda7586a8b89
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-04 07:24:35 -0500

    ADDED A IF STRUCTURE TO ANALYZE BUTTONS' CLICKS
    ADDED Events depending on clicks.

--- Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
+++ Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
@@ -4,18 +4,27 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    let hipodoge = document.getElementById('hipodoge').checked;
-    let capipepo = document.getElementById('capipepo').checked;
-    let ratigueya = document.getElementById('ratigueya').checked;
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
 
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodogue');
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo');
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya');
+    } else {
+        alert('Selecciona una mascota!')
+    }
 
+    /*
     if ((hipodoge == true) || (capipepo == true) || (ratigueya == true)) {
         alert('¡SELECCIONASTE TU MASCOTA!')
-    }
-
-    else {
+    } else {
         alert('Selecciona una mascota por favor!')
     }
+    */
     
 }
 
