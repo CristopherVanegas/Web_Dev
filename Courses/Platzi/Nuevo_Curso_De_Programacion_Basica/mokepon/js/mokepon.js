bc6e2bcef649eac729fcdf9b7892aa545f41a1d2
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-13 17:15:44 -0500

    ADDED randomness by adding a switch structure modifying the INNER.HTML and using RANDOMNESS function used in ROCK, PAPER and SCISSORS.

--- Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
+++ Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
@@ -3,12 +3,13 @@ function iniciarJuego() {
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)    
 }
 
+
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
-    
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
 
     if (inputHipodoge.checked) {
         alert('Seleccionaste a Hipodogue');
@@ -21,6 +22,38 @@ function seleccionarMascotaJugador() {
         spanMascotaJugador.innerHTML = 'Ratigueya';
     } else {
         alert('Selecciona una mascota!')
+    }
+
+    seleccionarMascotaEnemigo();
+}
+
+
+function aleatorio(min, max) {
+    return Math.floor( Math.random() * (max - min + 1) + min );
+}
+
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3);
+
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+    
+    switch (ataqueAleatorio) {
+        // Hipodoge
+        case 1:
+            spanMascotaEnemigo.innerHTML = 'Hipodoge';
+            break;
+
+        // Capipepo
+        case 2:
+            spanMascotaEnemigo.innerHTML = 'Capipepo';
+            break;
+
+        // Ratigueya
+        case 3:
+            spanMascotaEnemigo.innerHTML = 'Ratigueya';
+            break;
+
     }
 }
 
