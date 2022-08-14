9294ee0af5afe97110aa580c16d416e20bda87c5
CristopherVanegas <cristopher.platzi@gmail.com>
2022-08-14 14:14:34 -0500

    ADDED EVENTS ADDED functions for each attack. ADDED eventListeners for each attack button.

--- Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
+++ Courses/Platzi/Nuevo_Curso_De_Programacion_Basica/mokepon/js/mokepon.js
@@ -1,6 +1,38 @@
+
+let ataqueJugador;
+
+
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO';
+    alert(ataqueJugador);
+}
+
+function ataqueAgua() {
+    ataqueJugador = 'AGUA';
+    alert(ataqueJugador);
+}
+
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA';
+    alert(ataqueJugador);
+}
+
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)    
+
+    let botonFuego = document.getElementById('boton-fuego');
+    botonFuego.addEventListener('click', ataqueFuego);
+    
+    let botonAgua = document.getElementById('boton-agua');
+    botonAgua.addEventListener('click', ataqueAgua);
+
+    let botonTierra = document.getElementById('boton-tierra');
+    botonTierra.addEventListener('click', ataqueTierra);
+
+
+
 }
 
 
