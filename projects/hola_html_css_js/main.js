1775fb46b7a0278c79dbb6fef40af5e189fa8a5f
Cristopher Vanegas <cristophervanegas@Cristophers-MacBook-Pro.local>
2022-10-08 23:14:23 -0500

    ADDED useblackbox examen of how to create a navbar

--- projects/hola_html_css_js/main.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.button');
-const saludo = document.querySelector('.saludo');
-
-button.addEventListener('click', clickedButton);
-
-function clickedButton () {
-    saludo.classList.toggle('inactive');
-}
