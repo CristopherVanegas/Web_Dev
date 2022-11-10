ff0c5b5a95d2af9a14763361c07a69db8115d313
Cristopher Vanegas <cristopher.platzi@gmail.com>
2022-11-10 01:28:26 -0500

    ADDED a div for child options

--- projects/menu/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.button');
-const options = document.querySelector('.options');
-btn.addEventListener('click', dropdown);
-
-function dropdown() {
-    options.classList.toggle('inactive');
-}
