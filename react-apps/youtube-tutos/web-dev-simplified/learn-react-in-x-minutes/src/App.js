35202689a690a00165b1adbfe27e11c0148b1c85
Cristopher Vanegas <cristopher.platzi@gmail.com>
2023-01-04 23:20:30 -0500

    ADDED components for todos web app project and in root component useState is empty for default

--- react-apps/youtube-tutos/web-dev-simplified/learn-react-in-x-minutes/src/App.js
+++ react-apps/youtube-tutos/web-dev-simplified/learn-react-in-x-minutes/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from "./TodoList" // imports component TodoList from TodoList.js
 
 
 function App() {
+  useState([])
   return (
     <>  {/* empty element or fragment that returns more than one component bc the "wrapper or fragment" it's just one element but keeps more inside itself */}
       <TodoList />  {/*separated component, first element in our application.*/}
       <input type="text" />
       <button>Add Todo</button>
-      <button>Clear Completed Todos</button>
+      <button>Clear Completed</button>
       <div>0 left to do</div>
     </>
   );
