2bc39b390c7bde30969d7499d7b87fad04f3e939
Cristopher Vanegas <cristopher.platzi@gmail.com>
2023-01-07 00:58:20 -0500

    Added a new folder for useStateminiProject due to the error that was present in the last one

---
+++ react-apps/youtube-tutos/web-dev-simplified/use-state-in-15-minutes/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  function App() {
+    const [count, setCount] = useState(4)
+
+  }
+  return (
+    <>
+      <button>-</button>
+      <span>{count}</span>
+      <button>+</button>
+    </>
+  );
+}
+
+export default App;
