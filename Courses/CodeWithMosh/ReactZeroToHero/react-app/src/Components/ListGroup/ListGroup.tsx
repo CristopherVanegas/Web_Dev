e834a640b9ee2b2067afa068d104139dd12c61d6
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-17 16:04:47 -0500

    Commit with message

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -1,18 +1,20 @@
-
 function ListGroup() {
-    const items = [
-        'New York',
-        'Guayaquil',
-        'Medellin',
-        'Toronto',
-        'Tokyo'
-    ]
+    let items = ["New York", "Guayaquil", "Medellin", "Toronto", "Tokyo"];
+
+    items = [];
+    
+    const getMessage = () => {
+        return items.length === 0 ? <p>No item found</p> : null;
+    }
 
     return (
         <>
             <h1>List</h1>
+            {getMessage()}
             <ul className="list-group">
-                {items.map((item) => (<li key={item}>{item}</li>))}
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
             </ul>
         </>
     );
