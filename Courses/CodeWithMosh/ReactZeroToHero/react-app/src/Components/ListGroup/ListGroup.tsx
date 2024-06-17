ccf4802c40783f4c5f7187d4c77cda47f79707b6
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-17 12:18:38 -0500

    Rendering a list

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -1,20 +1,21 @@
-import { Fragment } from "react";
 
 function ListGroup() {
-  return (
-    <Fragment>
-        {/* or <> */}
-    <h1>List</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
-    </Fragment>
-    // or </>
-  );
+    const items = [
+        'New York',
+        'Guayaquil',
+        'Medellin',
+        'Toronto',
+        'Tokyo'
+    ]
+
+    return (
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                {items.map((item) => (<li key={item}>{item}</li>))}
+            </ul>
+        </>
+    );
 }
 
 export default ListGroup;
