b49e48bf84bfa865f1104e5180bba56c9ac2bd12
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-17 11:37:28 -0500

    Fragments

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -1,6 +1,10 @@
+import { Fragment } from "react";
+
 function ListGroup() {
   return (
-    <>
+    <Fragment>
+        {/* or <> */}
+    <h1>List</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
@@ -8,7 +12,8 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-    </>
+    </Fragment>
+    // or </>
   );
 }
 
