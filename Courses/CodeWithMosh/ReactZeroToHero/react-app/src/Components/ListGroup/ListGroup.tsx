05a1a2208afe922d8127ca73e5a4caad3a35460e
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-18 16:06:12 -0500

    Reinstalled the project and added a new way to **CONDITIONALY RENDER** the list!

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -1,16 +1,14 @@
+
 function ListGroup() {
     let items = ["New York", "Guayaquil", "Medellin", "Toronto", "Tokyo"];
 
     items = [];
-    
-    const getMessage = () => {
-        return items.length === 0 ? <p>No item found</p> : null;
-    }
 
     return (
         <>
             <h1>List</h1>
-            {getMessage()}
+            {/* {items.length === 0 ? <p>No item found</p> : null} */}
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item) => (
                     <li key={item}>{item}</li>
@@ -21,3 +19,4 @@ function ListGroup() {
 }
 
 export default ListGroup;
+
