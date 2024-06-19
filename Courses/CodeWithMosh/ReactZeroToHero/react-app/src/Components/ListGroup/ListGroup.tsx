bb3390ce3b25573902c1cb665c29e254cda4f093
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-19 14:45:17 -0500

    Added event handler lambda funcion into a variable and then it's called when onClick event is true.

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -1,22 +1,29 @@
+import { MouseEvent } from "react";
 
 function ListGroup() {
-    let items = ["New York", "Guayaquil", "Medellin", "Toronto", "Tokyo"];
+  const items = ["New York", "Guayaquil", "Medellin", "Toronto", "Tokyo"];
 
-    items = [];
+  // Handle Click
+  const handleClick = (event : MouseEvent) => console.log(event); 
 
-    return (
-        <>
-            <h1>List</h1>
-            {/* {items.length === 0 ? <p>No item found</p> : null} */}
-            {items.length === 0 && <p>No item found</p>}
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
-
