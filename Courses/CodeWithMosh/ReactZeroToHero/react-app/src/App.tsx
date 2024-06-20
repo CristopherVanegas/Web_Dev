2dca48150b9d1252bbd4838d698d3a64448b586b
Cristopher Vanegas <cristopher.platzi@gmail.com>
2024-06-20 12:18:41 -0500

    Passing funcions via props

--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/App.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/App.tsx
@@ -2,10 +2,11 @@ import ListGroup from "./Components/ListGroup/ListGroup";
 
 function App() {
   const items = ["New York", "Guayaquil", "Medellin", "Toronto", "Tokyo"];
+  const handleSelectItem = (item: string) => { console.log(item) };
 
   return (
     <div>
-      <ListGroup items={items} heading="List of Cities" />
+      <ListGroup items={items} heading="List of Cities" onSelectItem={handleSelectItem} />
     </div>
   );
 }
--- Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
+++ Courses/CodeWithMosh/ReactZeroToHero/react-app/src/Components/ListGroup/ListGroup.tsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 
 // To define the shape or input for a component
 interface Props {
-    items: string[];
-    heading: string;
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1)    // Hook
+function ListGroup({ items, heading, onSelectItem}: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Hook
   return (
     <>
       <h1>{heading}</h1>
@@ -16,9 +17,16 @@ function ListGroup({ items, heading }: Props) {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => {setSelectedIndex(index)}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
