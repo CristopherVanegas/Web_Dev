7100758ec86f48720b7afccf66ff12e4cc8a73bf
Cristopher Vanegas <itsmrjkali21@Cristophers-MacBook-Pro.local>
2024-09-08 18:32:18 -0500

    Added first class exercise, *ngFor

---
+++ Courses/Platzi/Angular/todoapp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Welcome';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ]
+  importNg = [
+    'import { CommonModule } from \'@angular/common\';',
+    'imports: [RouterOutlet, CommonModule],'
+  ]
+}
